b4880bb8fe0ac2cfe8df99afa11d4fc7cdefa63f
C19rk <clarkmanuntag@gmail.com>
2026-01-28 10:31:01 +0800

    fixed message again (removed msg on admin)

--- client/src/components/UserTotalOJTHours.jsx
+++ client/src/components/UserTotalOJTHours.jsx
@@ -38,6 +38,10 @@ export default function UserTotalOJTHours() {
     fetchMe();
   }, []);
 
+  if (role === "ADMIN") {
+    return null;
+  }
+
   if (loading) return <p>Loading User's Total OJT Hours...</p>;
   if (!loading && role !== "USER") {
     return null;
